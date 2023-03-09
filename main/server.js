73eb3efb2c74b7fece38ba8836d1bc78da48daee
will198735 <recinoswilfredo@gmail.com>
2023-03-08 22:39:41 -0800

    added the first inquirer function

--- main/server.js
+++ main/server.js
@@ -14,3 +14,44 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the company_db database.`)
 );
+const init = () => {
+  inquirer
+      .prompt([
+          {
+              type: "list",
+              message: "Please select from the following options:",
+              name: "initialize",
+              choices: [
+                  "View all departments",
+                  "View all roles",
+                  "View all employees",
+                  "Add a department",
+                  "Add a role",
+                  "Add an employee",
+                  "Update an employee role",
+                  "I'm finished"
+              ]
+          }
+      ]).then(answer => {
+        
+          switch (answer.initialize) {
+              case "View all departments": viewDept();
+                  break;
+              case "View all roles": viewRoles();
+                  break;
+              case "View all employees": viewEmployees();
+                  break;
+              case "Add a department": addDept();
+                  break;
+              case "Add a role": addRole();
+                  break;
+              case "Add an employee": addEmployee();
+                  break;
+              case "Update an employee role": updateEmployee();
+                  break;
+              case "I'm finished":
+                  console.log("Thank you very much!");
+                  process.exit();
+          }
+      }).catch(err => console.error(err));
+}
