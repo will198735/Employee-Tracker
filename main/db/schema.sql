045bcda0d6b0c2773e26cef3310039a2b4723b5e
will198735 <recinoswilfredo@gmail.com>
2023-03-08 16:24:36 -0800

    modified schema

--- main/db/schema.sql
+++ main/db/schema.sql
@@ -1,37 +1,29 @@
-DROP DATABASE IF EXISTS tracker_db;
-
-CREATE DATABASE  tracker_db;
-
-USE  tracker_db;
-
-
-CREATE TABLE departments(
-    id INT  UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
-);
+DROP DATABASE if EXISTS company_db;
+CREATE DATABASE company_db;
+ 
+ USE company_db; 
 
 
-CREATE TABLE roles (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL UNSIGNED NOT NULL,
-    departments_id INT UNSIGNED NOT NULL,
-INDEX dep_id (departments_id),
-   CONSTRAINT fk_departments FOREIGN KEY (departments_id)
-     REFERENCES departments (id)
-     ON DELETE  CASCADE
-);
+ CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 
+ );
 
-CREATE TABLE employees (
-     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-     first_name VARCHAR(30) NOT NULL,
-     last_name VARCHAR(30) NOT NULL,
-     roles_id INT UNSIGNED NOT NULL,
-     INDEX role_id (roles_id),
-     CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id)ON DELETE CASCADE,
-      manager_id INT UNSIGNED,
-      INDEX manager_ind (manager_id),
-      CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
-   
-);
+ CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    Foreign Key (department_id) REFERENCES departments (id) ON DELETE SET NULL
+ );
+
+ CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    Foreign Key (role_id) REFERENCES roles (id) ON DELETE SET NULL,
+    Foreign Key (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+ );
